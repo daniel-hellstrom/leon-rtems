3edb24cbdde7293a329715db74c624dd1a7b026a
Daniel Hellstrom <daniel@gaisler.com>
2011-11-09 16:13:29 +0100

    LEON3BSP MP: removed unused code

--- c/src/lib/libbsp/sparc/leon3/shmsupp/getcfg.c
+++ c/src/lib/libbsp/sparc/leon3/shmsupp/getcfg.c
@@ -79,8 +79,8 @@ extern rtems_mpci_entry Shm_Send_packet(
 /* Let user override this configuration by declaring this a weak variable */
 shm_config_table BSP_shm_cfgtbl __attribute__((weak)) =
 {
-	0x40000000,
-	0x00001000,
+  0x40000000,
+  0x00001000,
 };
 
 void Shm_Get_configuration(
@@ -92,10 +92,6 @@ void Shm_Get_configuration(
   int i;
   unsigned int tmp;
 
-#if 0
-  BSP_shm_cfgtbl.base         = 0x40000000;
-  BSP_shm_cfgtbl.length       = 0x00010000;
-#endif
   BSP_shm_cfgtbl.format       = SHM_BIG;
 
   /*
