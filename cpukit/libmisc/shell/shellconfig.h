84abf8ca0cace3dbab46f11193be874aa9c84cf6
Daniel Hellstrom <daniel@gaisler.com>
2011-11-28 10:25:15 +0100

    LIBPCI: added PCI shell command
    Signed-off-by: Daniel Hellstrom <daniel@gaisler.com>

--- cpukit/libmisc/shell/shellconfig.h
+++ cpukit/libmisc/shell/shellconfig.h
@@ -80,6 +80,11 @@ extern rtems_shell_cmd_t rtems_shell_MALLOC_INFO_Command;
   extern rtems_shell_cmd_t rtems_shell_NETSTATS_Command;
 #endif
 
+/*
+ *  Extern for System commands
+ */
+extern rtems_shell_cmd_t rtems_shell_PCI_Command;
+
 extern rtems_shell_cmd_t *rtems_shell_Initial_commands[];
 
 /*
@@ -422,6 +427,17 @@ extern rtems_shell_alias_t *rtems_shell_Initial_aliases[];
           && !defined(CONFIGURE_SHELL_NO_COMMAND_RTC)) \
         || defined(CONFIGURE_SHELL_COMMAND_RTC)
       &rtems_shell_RTC_Command,
+    #endif
+
+    /*
+     *  System related commands
+     */
+    #if defined(RTEMS_PCI_CONFIG_LIB)
+      #if (defined(CONFIGURE_SHELL_COMMANDS_ALL) && \
+           !defined(CONFIGURE_SHELL_NO_COMMAND_PCI)) || \
+          defined(CONFIGURE_SHELL_COMMAND_PCI)
+        &rtems_shell_PCI_Command,
+      #endif
     #endif
 
     /*
