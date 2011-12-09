15b8a68fd929916bf419bb5e88166a286207475e
Daniel Hellstrom <daniel@gaisler.com>
2011-12-09 15:51:05 +0100

    LEON3: debugputs synced with clean branch. use ambapp_find_by_idx

--- c/src/lib/libbsp/sparc/leon3/console/debugputs.c
+++ c/src/lib/libbsp/sparc/leon3/console/debugputs.c
@@ -1,5 +1,7 @@
 /*
- *  Console Debug interface driver for printk()
+ *  This file contains the TTY driver for the serial ports on the LEON.
+ *
+ *  This driver uses the termios pseudo driver.
  *
  *  COPYRIGHT (c) 1989-1999.
  *  On-Line Applications Research Corporation (OAR).
@@ -29,127 +31,121 @@
  * ...
  */
 int debug_uart_index __attribute__((weak)) = 0;
-
 ambapp_apb_uart *dbg_uart = NULL;
 
-/* Before UART driver has registered calls to printk that gets to
- * console_dbg_out_char() will be filling data into the pre_printk_dbgbuf[]
- * buffer, hopefully the buffer can help in debugging the cause. At least
- * the last printk() will be caught.
+/* Before UART driver has registered (or when no UART is available), calls to
+ * printk that gets to bsp_out_char() will be filling data into the
+ * pre_printk_dbgbuf[] buffer, hopefully the buffer can help debugging the
+ * early BSP boot.. At least the last printk() will be caught.
  */
 char pre_printk_dbgbuf[32] = {0};
 int pre_printk_pos = 0;
 
-/* This routine polls for one character, return EOF if no char is available */
-int apbuart_inbyte_nonblocking(ambapp_apb_uart *regs)
+/* Initialize the BSP system debug console layer. It will scan AMBA Plu&Play
+ * for a debug APBUART and enable RX/TX for that UART.
+ */
+int bsp_debug_uart_init(void)
 {
-	if (regs->status & LEON_REG_UART_STATUS_ERR)
-		regs->status = ~LEON_REG_UART_STATUS_ERR;
+  int i;
+  struct ambapp_dev *adev;
+  struct ambapp_apb_info *apb;
 
-	if ((regs->status & LEON_REG_UART_STATUS_DR) == 0)
-		return EOF;
+  /* Update debug_uart_index to index used as debug console.
+   * Let user select Debug console by setting debug_uart_index. If the
+   * BSP is to provide the default UART (debug_uart_index==0):
+   *   non-MP: APBUART[0] is debug console
+   *   MP: LEON CPU index select UART
+   */
+  if (debug_uart_index == 0) {
+#if defined(RTEMS_MULTIPROCESSING)
+    debug_uart_index = LEON3_Cpu_Index;
+#else
+    debug_uart_index = 0;
+#endif
+  } else {
+    debug_uart_index = debug_uart_index - 1; /* User selected dbg-console */
+  }
 
-	return (int)regs->data;
+  /* Find APBUART core for System Debug Console */
+  i = debug_uart_index;
+  adev = (void *)ambapp_for_each(&ambapp_plb, (OPTIONS_ALL|OPTIONS_APB_SLVS),
+                                 VENDOR_GAISLER, GAISLER_APBUART,
+                                 ambapp_find_by_idx, (void *)&i);
+  if (adev) {
+    /* Found a matching debug console, initialize debug uart if present
+     * for printk
+     */
+    apb = (struct ambapp_apb_info *)adev->devinfo;
+    dbg_uart = (ambapp_apb_uart *)apb->start;
+    dbg_uart->ctrl |= LEON_REG_UART_CTRL_RE | LEON_REG_UART_CTRL_TE;
+    dbg_uart->status = 0;
+    return 1;
+  } else
+    return 0;
 }
 
-/* This routine transmits a character, it will busy-wait until on character
- * fits in the APBUART Transmit FIFO
+/*
+ *  apbuart_outbyte_polled
+ *
+ *  This routine transmits a character using polling.
  */
 void apbuart_outbyte_polled(
   ambapp_apb_uart *regs,
   unsigned char ch,
   int do_cr_on_newline,
-  int wait_sent)
+  int wait_sent
+)
 {
 send:
-	while ((regs->status & LEON_REG_UART_STATUS_THE) == 0) {
-		/* Lower bus utilization while waiting for UART */
-		asm volatile ("nop"::);	asm volatile ("nop"::);
-		asm volatile ("nop"::);	asm volatile ("nop"::);
-		asm volatile ("nop"::);	asm volatile ("nop"::);
-		asm volatile ("nop"::);	asm volatile ("nop"::);
-	}
-	regs->data = (unsigned int) ch;
+  while ( (regs->status & LEON_REG_UART_STATUS_THE) == 0 ) {
+    /* Lower bus utilization while waiting for UART */
+    asm volatile ("nop"::); asm volatile ("nop"::);
+    asm volatile ("nop"::); asm volatile ("nop"::);
+    asm volatile ("nop"::); asm volatile ("nop"::);
+    asm volatile ("nop"::); asm volatile ("nop"::);
+  }
+  regs->data = (unsigned int) ch;
 
-	if ((ch == '\n') && do_cr_on_newline) {
-		ch = '\r';
-		goto send;
-	}
+  if ((ch == '\n') && do_cr_on_newline) {
+    ch = '\r';
+    goto send;
+  }
 
-	/* Wait until the character has been sent? */
-	if (wait_sent) {
-		while ((regs->status & LEON_REG_UART_STATUS_THE) == 0)
-			;
-	}
+  /* Wait until the character has been sent? */
+  if (wait_sent) {
+    while ((regs->status & LEON_REG_UART_STATUS_THE) == 0)
+      ;
+  }
 }
 
-/* AMBA PP find routine. Extract APBUART information into data structure. */
-int find_matching_debug_apbuart(struct ambapp_dev *dev, int index, void *arg)
+/*
+ *  apbuart_inbyte_nonblocking
+ *
+ *  This routine polls for a character.
+ */
+int apbuart_inbyte_nonblocking(ambapp_apb_uart *regs)
 {
-	struct ambapp_apb_info *apb;
+  /* Clear errors */
+  if (regs->status & LEON_REG_UART_STATUS_ERR)
+    regs->status = ~LEON_REG_UART_STATUS_ERR;
 
-	if (debug_uart_index == 0)
-		return 1;
-
-	if (--debug_uart_index != 0)
-		return 0;
-
-	apb = (struct ambapp_apb_info *)dev->devinfo;
-
-	/* Extract needed information of one APBUART */
-	dbg_uart = (ambapp_apb_uart *)apb->start;
-
-	/* We assume that the baud rate and transmission paramters already have
-	 * been configured, but the transmitter/receiver is enabled once on
-	 * debug UART initialization
-	 */
-	dbg_uart->ctrl |= LEON_REG_UART_CTRL_RE | LEON_REG_UART_CTRL_TE;
-
-	return 1; /* stop search */
+  if ((regs->status & LEON_REG_UART_STATUS_DR) == 0)
+    return EOF;
+  else
+    return (int) regs->data;
 }
-
-/* Scan AMBA Plu&Play for debug APBUART */
-void bsp_debug_uart_init(void)
-{
-	if (debug_uart_index == 0)
-#if defined(RTEMS_MULTIPROCESSING)
-	/* On RTEMS AMP systems, CPU0 use APBUART0, CPU1 use APBUART1 ...
-	 * unless user has overrided default configuration
-	 */
-		debug_uart_index = LEON3_Cpu_Index + 1;
-#else
-		debug_uart_index = 1;
-#endif
-
-	/* Find APBUART core */
-	ambapp_for_each(&ambapp_plb, (OPTIONS_ALL|OPTIONS_APB_SLVS),
-			VENDOR_GAISLER, GAISLER_APBUART,
-			find_matching_debug_apbuart, NULL);
-}
-
 
 /* putchar/getchar for printk */
 static void bsp_out_char(char c)
 {
-	if (dbg_uart == NULL) {
-		/* Local debug buffer when UART driver has not registered */
-		pre_printk_dbgbuf[pre_printk_pos++] = c;
-		pre_printk_pos = pre_printk_pos & (sizeof(pre_printk_dbgbuf)-1);
-		return;
-	}
-	apbuart_outbyte_polled(dbg_uart, c, 1, 1);
-}
+  if (dbg_uart == NULL) {
+    /* Local debug buffer when UART driver has not registered */
+    pre_printk_dbgbuf[pre_printk_pos++] = c;
+    pre_printk_pos = pre_printk_pos & (sizeof(pre_printk_dbgbuf)-1);
+    return;
+  }
 
-static int bsp_in_char(void)
-{
-	int tmp;
-
-	if (dbg_uart == NULL)
-		return EOF;
-
-	while ((tmp = apbuart_inbyte_nonblocking(dbg_uart)) < 0)
-		;
-	return tmp;
+  apbuart_outbyte_polled(dbg_uart, c, 1, 1);
 }
 
 /*
@@ -159,4 +155,17 @@ static int bsp_in_char(void)
 #include <rtems/bspIo.h>
 
 BSP_output_char_function_type BSP_output_char = bsp_out_char;
+
+static int bsp_in_char(void)
+{
+  int tmp;
+
+  if (dbg_uart == NULL)
+    return EOF;
+
+  while ((tmp = apbuart_inbyte_nonblocking(dbg_uart)) < 0)
+    ;
+  return tmp;
+}
+
 BSP_polling_getchar_function_type BSP_poll_char = bsp_in_char;
